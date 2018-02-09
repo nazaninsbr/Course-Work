65d8347617783bcc00c18e23639a3682054cfdbe
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-02-09 18:00:34 +0330

    some stuff on rethinkdb and some minor changes in course completion lists

---
+++ Subject Based/database/rethinkdb/configBasedOnTut.py
@@ -0,0 +1,27 @@
+import rethinkdb as r
+
+r.connect( "localhost", 28015).repl()
+r.db("test").table_create("authors").run()
+r.table("authors").insert([
+    { "name": "William Adama", "tv_show": "Battlestar Galactica",
+      "posts": [
+        {"title": "Decommissioning speech", "content": "The Cylon War is long over..."},
+        {"title": "We are at war", "content": "Moments ago, this ship received..."},
+        {"title": "The new Earth", "content": "The discoveries of the past few days..."}
+      ]
+    },
+    { "name": "Laura Roslin", "tv_show": "Battlestar Galactica",
+      "posts": [
+        {"title": "The oath of office", "content": "I, Laura Roslin, ..."},
+        {"title": "They look like us", "content": "The Cylons have the ability..."}
+      ]
+    },
+    { "name": "Jean-Luc Picard", "tv_show": "Star Trek TNG",
+      "posts": [
+        {"title": "Civil rights", "content": "There are some words I've known since..."}
+      ]
+    }
+]).run()
+cursor = r.table("authors").run()
+for document in cursor:
+    print(document)
